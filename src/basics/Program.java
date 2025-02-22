0a543093c27292a1b77fe86cfddc8b465accc118
junyorff12 <junyorff12@gmail.com>
2025-02-22 11:47:56 -0300

    criando classe para representar a entidade Triangulo

--- src/basics/Program.java
+++ src/basics/Program.java
@@ -3,6 +3,8 @@ package basics;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Triangle;
+
 public class Program {
 
 	public static void main(String[] args) {
@@ -10,31 +12,39 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		double xA, xB, xC, yA, yB, yC;
+		Triangle x, y;
+		
+		x = new Triangle();
+		y = new Triangle();
+		
+		
 		System.out.println("Enter with the measures of the triangle X");
-		xA = sc.nextDouble();
-		xB = sc.nextDouble();
-		xC = sc.nextDouble();
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
+		
 		System.out.println("Enter with the measures of the triangle Y");
-
-		yA = sc.nextDouble();
-		yB = sc.nextDouble();
-		yC = sc.nextDouble();
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
 		
-		double p = (xA + xB + xC) / 2;
-		double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+		double p = (x.a + x.b + x.c) / 2;
+		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
 		
-		p = (yA + yB + yC) / 2;
-		double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+		p = (y.a + y.b + y.c) / 2;
+		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+		
 		
 		System.out.printf("Area triangle x: %.4f%n", areaX);
 		System.out.printf("Area triangle y: %.4f%n", areaY);
 		
 		if(areaX > areaY) {
-			System.out.println("Area X larger!");
+			System.out.println("Area of triangle X larger!");
 		}else {
-			System.out.println("Area Y larger!");
+			System.out.println("Area of trinagle Y larger!");
 		}
+		
+		sc.close();
 
 	}
 
